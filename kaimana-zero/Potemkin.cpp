8d4d4c3c2c6accf9279f0ac806fc01e104c68145
Hunter Nelson <57781976+mushinsha@users.noreply.github.com>
2025-04-21 16:28:23 -0700

    Update Potemkin.cpp
    Added moves and animations for each move

--- kaimana-zero/Potemkin.cpp
+++ kaimana-zero/Potemkin.cpp
@@ -36,118 +36,128 @@
 // Define combo sequences here and corresponding animations. Most complex moves should go first (eg, supers, ultras)
 bool Potemkin::testForCharacterCombos() const
 {
-  //Super Dragon punch (both sides)
-  if( kaimana.switchHistoryTest( COMBO_DOUBLE_QUARTERCIRCLE_RIGHT, DOUBLE_QUARTERCIRCLE_INPUT_COUNT, K1Array, 1, false ) ||
-      kaimana.switchHistoryTest( COMBO_DOUBLE_QUARTERCIRCLE_RIGHT, DOUBLE_QUARTERCIRCLE_INPUT_COUNT, K2Array, 1, false ) ||
-      kaimana.switchHistoryTest( COMBO_DOUBLE_QUARTERCIRCLE_RIGHT, DOUBLE_QUARTERCIRCLE_INPUT_COUNT, K3Array, 1, false ) ||
-      kaimana.switchHistoryTest( COMBO_DOUBLE_QUARTERCIRCLE_LEFT, DOUBLE_QUARTERCIRCLE_INPUT_COUNT, K1Array, 1, false ) ||
-      kaimana.switchHistoryTest( COMBO_DOUBLE_QUARTERCIRCLE_LEFT, DOUBLE_QUARTERCIRCLE_INPUT_COUNT, K2Array, 1, false ) ||
-      kaimana.switchHistoryTest( COMBO_DOUBLE_QUARTERCIRCLE_LEFT, DOUBLE_QUARTERCIRCLE_INPUT_COUNT, K3Array, 1, false ))
+
+  //OVERDRIVES
+  //Heavenly Potemkin Buster [641236 or 412364 + S]
+  if( kaimana.switchHistoryTest( COMBO_TYRANTRAVE_RIGHT, TYRANTRAVE_INPUT_COUNT, P2Array, 1, false ) ||
+    ( kaimana.switchHistoryTest( COMBO_TYRANTRAVE_LEFT, TYRANTRAVE_INPUT_COUNT, P2Array, 1, false ) )
+  {
+    FlashAllSpeedIncreasing_Combo_Animation(RED);
+    FlashAllSpeedIncreasing_Combo_Animation(WHITE);
+    FlashAllSpeedIncreasing_Combo_Animation(BLUE);   
+    return true;
+  }
+
+  //Gaiganter Kai [641236 or 412364 + HS]
+  if( kaimana.switchHistoryTest( COMBO_TYRANTRAVE_RIGHT, TYRANTRAVE_INPUT_COUNT, K2Array, 1, false ) ||
+    ( kaimana.switchHistoryTest( COMBO_TYRANTRAVE_LEFT, TYRANTRAVE_INPUT_COUNT, K2Array, 1, false ) )
+  {
+    FlashAllSpeedIncreasing_Combo_Animation(GREEN);
+    FlashAllSpeedIncreasing_Combo_Animation(GOLD);
+    FlashAllSpeedIncreasing_Combo_Animation(GREEN);
+    return true;
+  }
+
+  
+  //SPECIAL MOVES
+  //Potemkin Buster [641236 or 412364 + P]
+  if( kaimana.switchHistoryTest( COMBO_TYRANTRAVE_RIGHT, TYRANTRAVE_INPUT_COUNT, P1Array, 1, false ) ||
+    ( kaimana.switchHistoryTest( COMBO_TYRANTRAVE_LEFT, TYRANTRAVE_INPUT_COUNT, P1Array, 1, false ) )
   {
     FlashAllSpeedIncreasing_Combo_Animation(WHITE);
+    FlashAllSpeedIncreasing_Combo_Animation(ORANGE);
+    FlashAllSpeedIncreasing_Combo_Animation(BLUE);   
     return true;
   }
   
-  //Super fireball right
-  if( kaimana.switchHistoryTest( COMBO_DOUBLE_QUARTERCIRCLE_RIGHT, DOUBLE_QUARTERCIRCLE_INPUT_COUNT, P1Array, 1, false ) ||
-      kaimana.switchHistoryTest( COMBO_DOUBLE_QUARTERCIRCLE_RIGHT, DOUBLE_QUARTERCIRCLE_INPUT_COUNT, P2Array, 1, false ) ||
-      kaimana.switchHistoryTest( COMBO_DOUBLE_QUARTERCIRCLE_RIGHT, DOUBLE_QUARTERCIRCLE_INPUT_COUNT, P3Array, 1, false ) )
+  
+  //Mega Fist (both directions) [236 + P]
+  if( kaimana.switchHistoryTest( COMBO_QUARTERCIRCLE_RIGHT, QUARTERCIRCLE_INPUT_COUNT, P1Array, 1, false ) ||
+    ( kaimana.switchHistoryTest( COMBO_QUARTERCIRCLE_LEFT, QUARTERCIRCLE_INPUT_COUNT, P1Array, 1, false ) )
   {
-    WaveEffect_Combo_Animation(EFT_LeftToRight, EFS_Medium, 2, CYAN);  
+    WaveEffect_Combo_Animation(EFT_DownToUp, EFS_Medium, 2, RED);    
     return true;
   }
+  
+  
+  //Slide Head (both directions) [236 + S]
+  if( kaimana.switchHistoryTest( COMBO_QUARTERCIRCLE_RIGHT, QUARTERCIRCLE_INPUT_COUNT, P2Array, 1, false ) ||
+    ( kaimana.switchHistoryTest( COMBO_QUARTERCIRCLE_LEFT, QUARTERCIRCLE_INPUT_COUNT, P2Array, 1, false ) )
+  {
+    WaveEffect_Combo_Animation(EFT_UpToDown, EFS_Slow, 0, GREEN);    
+    return true;
+  }
+  
+  
+  //Heat Knuckle (both directions) [623 or 421 + HS]
+  if( kaimana.switchHistoryTest( COMBO_DP_RIGHT, DP_INPUT_COUNT, K2Array, 1, false ) ||
+      kaimana.switchHistoryTest( COMBO_DP_LEFT, DP_INPUT_COUNT, K2Array, 1, false ) )
+  {
+    FlashColour_Combo_Animation(RED, 250);
+    return true;
+  }
+
+
+  //Heat Tackle (both directions) [623 or 421 + K]
+  if( kaimana.switchHistoryTest( COMBO_DP_RIGHT, DP_INPUT_COUNT, K1Array, 1, false ) ||
+      kaimana.switchHistoryTest( COMBO_DP_LEFT, DP_INPUT_COUNT, K1Array, 1, false ) )
+  {
+    WaveEffect_Combo_Animation(EFT_DownToUp, EFS_Fast, 0, WHITE);
+    WaveEffect_Combo_Animation(EFT_DownToUp, EFS_Medium, 0, RED);
+    return true;
+  }
+
+  
+  //Hammer Fall (right) [(4)6 + HS]
+  if( kaimana.switchHistoryTest( COMBO_CHARGE_LEFT_RIGHT, CHARGE_INPUT_COUNT, K2Array, 1, true ) )
+  {
+    WaveEffect_Combo_Animation(EFT_LeftToRight, EFS_Medium, 0, ORANGE);
+    WaveEffect_Combo_Animation(EFT_RightToLeft, EFS_Fast, 0, WHITE);
+    return true;
+  }
+
+  //Hammer Fall (left) [(6)4 + HS]
+  if( kaimana.switchHistoryTest( COMBO_CHARGE_RIGHT_LEFT, CHARGE_INPUT_COUNT, K1Array, 1, true ) )
+  {
+    WaveEffect_Combo_Animation(EFT_RightToLeft, EFS_Medium, 0, ORANGE);
+      WaveEffect_Combo_Animation(EFT_LeftToRight, EFS_Fast, 0, WHITE);
+    return true;
+  }
+
     
-  //Super fireball left
-  if( kaimana.switchHistoryTest( COMBO_DOUBLE_QUARTERCIRCLE_LEFT, DOUBLE_QUARTERCIRCLE_INPUT_COUNT, P1Array, 1, false ) ||
-      kaimana.switchHistoryTest( COMBO_DOUBLE_QUARTERCIRCLE_LEFT, DOUBLE_QUARTERCIRCLE_INPUT_COUNT, P2Array, 1, false ) ||
-      kaimana.switchHistoryTest( COMBO_DOUBLE_QUARTERCIRCLE_LEFT, DOUBLE_QUARTERCIRCLE_INPUT_COUNT, P3Array, 1, false ) )
+  //Garuda Impact (right) [214 + HS]
+  if( kaimana.switchHistoryTest( COMBO_QUARTERCIRCLE_LEFT, QUARTERCIRCLE_INPUT_COUNT, K2Array, 1, false ) )
   {
-    WaveEffect_Combo_Animation(EFT_RightToLeft, EFS_Medium, 2, CYAN);  
+    WaveEffect_Combo_Animation(EFT_LeftToRight, EFS_Medium, 0, ORANGE);
+    WaveEffect_Combo_Animation(EFT_LeftToRight, EFS_Fast, 0, RED);
+    return true;
+  }
+
+  //Garuda Impact (left) [236 + HS]
+  if( kaimana.switchHistoryTest( COMBO_QUARTERCIRCLE_RIGHT, QUARTERCIRCLE_INPUT_COUNT, K2Array, 1, false ) )
+  {
+    WaveEffect_Combo_Animation(EFT_RightToLeft, EFS_Medium, 0, ORANGE);
+    WaveEffect_Combo_Animation(EFT_RightToLeft, EFS_Fast, 0, RED);
     return true;
   }
 
 
-  //Dragon punch right
-  if( kaimana.switchHistoryTest( COMBO_DP_LEFT, DP_INPUT_COUNT, P1Array, 1, false ) )
+  //FDB (right) [214 + K]
+  if( kaimana.switchHistoryTest( COMBO_QUARTERCIRCLE_LEFT, QUARTERCIRCLE_INPUT_COUNT, K1Array, 1, false ) )
   {
-    FlashColour_Combo_Animation(WHITE, 250);
-    return true;
-  }
-  if( kaimana.switchHistoryTest( COMBO_DP_LEFT, DP_INPUT_COUNT, P2Array, 1, false ) )
-  {
-    FlashColour_Combo_Animation(WHITE, 500);
-    return true;
-  }
-  if( kaimana.switchHistoryTest( COMBO_DP_LEFT, DP_INPUT_COUNT, P3Array, 1, false ) )
-  {
-    FlashColour_Combo_Animation(WHITE, 750);
-    return true;
-  }
-    
-  //Dragon punch left
-  if( kaimana.switchHistoryTest( COMBO_DP_RIGHT, DP_INPUT_COUNT, P1Array, 1, false ) )
-  {
-    FlashColour_Combo_Animation(WHITE, 250);
-    return true;
-  }
-  if( kaimana.switchHistoryTest( COMBO_DP_RIGHT, DP_INPUT_COUNT, P2Array, 1, false ) )
-  {
-    FlashColour_Combo_Animation(WHITE, 500);
-    return true;
-  }
-  if( kaimana.switchHistoryTest( COMBO_DP_RIGHT, DP_INPUT_COUNT, P3Array, 1, false ) )
-  {
-    FlashColour_Combo_Animation(WHITE, 750);
+    WaveEffect_Combo_Animation(EFT_LeftToRight, EFS_Medium, 0, ORANGE);
+    WaveEffect_Combo_Animation(EFT_LeftToRight, EFS_Fast, 0, WHITE);
     return true;
   }
 
-
-  //Fireball to the right
-  if( kaimana.switchHistoryTest( COMBO_QUARTERCIRCLE_RIGHT, QUARTERCIRCLE_INPUT_COUNT, P1Array, 1, false ) )
+  //FDB (left) [236 + K]
+  if( kaimana.switchHistoryTest( COMBO_QUARTERCIRCLE_RIGHT, QUARTERCIRCLE_INPUT_COUNT, K1Array, 1, false ) )
   {
-    WaveEffect_Combo_Animation(EFT_LeftToRight, EFS_Slow, 0, CYAN);  
-    return true;
-  }
-  if( kaimana.switchHistoryTest( COMBO_QUARTERCIRCLE_RIGHT, QUARTERCIRCLE_INPUT_COUNT, P2Array, 1, false ) )
-  {
-    WaveEffect_Combo_Animation(EFT_LeftToRight, EFS_Medium, 0, CYAN);  
-    return true;
-  }
-  if( kaimana.switchHistoryTest( COMBO_QUARTERCIRCLE_RIGHT, QUARTERCIRCLE_INPUT_COUNT, P3Array, 1, false ) )
-  {
-    WaveEffect_Combo_Animation(EFT_LeftToRight, EFS_Fast, 0, CYAN);  
+    WaveEffect_Combo_Animation(EFT_RightToLeft, EFS_Medium, 0, ORANGE);
+    WaveEffect_Combo_Animation(EFT_RightToLeft, EFS_Fast, 0, WHITE);
     return true;
   }
 
-  //Fireball to the left
-  if( kaimana.switchHistoryTest( COMBO_QUARTERCIRCLE_LEFT, QUARTERCIRCLE_INPUT_COUNT, P1Array, 1, false ) )
-  {
-    WaveEffect_Combo_Animation(EFT_RightToLeft, EFS_Slow, 0, CYAN);  
-    return true;
-  }
-  if( kaimana.switchHistoryTest( COMBO_QUARTERCIRCLE_LEFT, QUARTERCIRCLE_INPUT_COUNT, P2Array, 1, false ) )
-  {
-    WaveEffect_Combo_Animation(EFT_RightToLeft, EFS_Medium, 0, CYAN);  
-    return true;
-  }
-  if( kaimana.switchHistoryTest( COMBO_QUARTERCIRCLE_LEFT, QUARTERCIRCLE_INPUT_COUNT, P3Array, 1, false ) )
-  {
-    WaveEffect_Combo_Animation(EFT_RightToLeft, EFS_Fast, 0, CYAN);  
-    return true;
-  }
-
-
-  //Hurricane kick right
-  if( kaimana.switchHistoryTest( COMBO_QUARTERCIRCLE_RIGHT, QUARTERCIRCLE_INPUT_COUNT, K1Array, 1, false ) ||
-      kaimana.switchHistoryTest( COMBO_QUARTERCIRCLE_RIGHT, QUARTERCIRCLE_INPUT_COUNT, K2Array, 1, false ) ||
-      kaimana.switchHistoryTest( COMBO_QUARTERCIRCLE_RIGHT, QUARTERCIRCLE_INPUT_COUNT, K3Array, 1, false ) ||
-      kaimana.switchHistoryTest( COMBO_QUARTERCIRCLE_LEFT, QUARTERCIRCLE_INPUT_COUNT, K1Array, 1, false ) ||
-      kaimana.switchHistoryTest( COMBO_QUARTERCIRCLE_LEFT, QUARTERCIRCLE_INPUT_COUNT, K2Array, 1, false ) ||
-      kaimana.switchHistoryTest( COMBO_QUARTERCIRCLE_LEFT, QUARTERCIRCLE_INPUT_COUNT, K3Array, 1, false ) )
-  {
-    Circle_OneColour_Combo_Animation(1, WHITE);  
-    return true;
-  }
-
+  
   return false;
 } 
