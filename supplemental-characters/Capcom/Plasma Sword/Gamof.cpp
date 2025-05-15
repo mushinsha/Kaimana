8fbdb688448ff0569a249471afbfb0c1a45fb173
Hunter Nelson <57781976+mushinsha@users.noreply.github.com>
2025-05-15 10:03:22 -0700

    Added Plasma Sword roster

---
+++ supplemental-characters/Capcom/Plasma Sword/Gamof.cpp
@@ -0,0 +1,56 @@
+//  Gamof.cpp
+//
+//  Copyright 2023 Paradise Arcade Shop, ParadiseArcadeShop.com  
+//  All rights reserved.  Use is subject to license terms.
+//
+//  Code is provided for entertainment purposes and use with the Kaimana controller.
+//  Code may be copied, modified, resused with this Copyright notice.
+//  No commercial use without written permission from Paradise Arcade Shop.
+//
+//  Paradise Arcade Shop Kaimana LED Driver Board
+//  Initial Release October 15, 2013
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//  THE SOFTWARE.
+//
+//  Kaimana Gamof character combo checks and animation calls. First released by ParadiseArcadeShop.com November, 2023
+//  Characters are added into the character selection array in the main ino file. See that for info on how characters are selected
+//
+//  Created:  April 2025 by Hunter "Mushinsha" Nelson
+//
+
+#define __PROG_TYPES_COMPAT__
+#include <avr/io.h>
+#include <avr/pgmspace.h>
+#include <util/delay.h>
+#include "kaimana.h"
+#include "kaimana_custom.h"
+#include "animations.h"
+#include "Characters.h"
+
+// Define combo sequences here and corresponding animations. Most complex moves should go first (eg, supers, ultras)
+bool Gamof::testForCharacterCombos() const
+{
+  
+//MOVELIST BELOW
+
+//SUPER ARTS
+  //Raishin Mahha Ken           [236236 + P] (SA1)
+  //Tenshin Senkyuutai          [236236 + K] (SA2)
+  //Sei'ei Enbu                 [236236 + P] (SA3)
+  
+//SPECIAL MOVES
+  //Tourou Zan                  [236 + P]
+  //Senkyuutai                  [236 + K]
+  //Byakko Soushouda            [214 + P]
+  //Fake Byakko Soushouda       [214 + PP]
+  //Zenpou Tenshin              [63214 + K]
+  //Kaihou                      [623 + K]
+  
+  return false;
+} 
