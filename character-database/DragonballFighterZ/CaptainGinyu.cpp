8695002d510b4af02ee8f6b33fa9f2f392d5c4f8
Hunter Nelson <57781976+mushinsha@users.noreply.github.com>
2026-08-18 16:50:57 -0700

    Create CaptainGinyu.cpp

---
+++ character-database/DragonballFighterZ/CaptainGinyu.cpp
@@ -0,0 +1,33 @@
+//  CaptainGinyu.cpp
+//
+//  Created:  August 2026 by Hunter "Mushinsha" Nelson
+//
+
+#define __PROG_TYPES_COMPAT__
+#include <avr/io.h>
+#include <avr/pgmspace.h>
+#include <util/delay.h>
+#include "kaimana.h"
+#include "kaimana_custom.h"
+#include "animations.h"
+#include "Characters.h"
+
+// Define combo sequences here and corresponding animations. Most complex moves should go first (eg, supers, ultras)
+bool CaptainGinyu::testForCharacterCombos() const
+{
+  
+//MOVELIST BELOW
+
+//OVERDRIVES
+  //Level 1 Super                           [236 + L+M]
+  //Level 2 Super                           [236 + H+S]
+  //Level 3 Super                           [236 + L+M or H+S]
+  
+
+//SPECIAL MOVES
+  //Shield Strike                           [236 + L/M/H]
+  //Soaring Justice                         [623 + L/M/H]
+  //Shield Shock                            [22 + L/M/H]
+  
+  return false;
+} 
